a9105bfaa59c29ea10aa7fe2655f96d7bd00e2e3
erin <erin314@naver.com>
2015-04-11 20:11:16 +0900

    source update

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost/test1');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var play = require('./routes/play');
@@ -19,14 +23,21 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/play', play);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// show All users
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            userlist : docs
+        });
+    	// res.json(docs);
+    });
 });
 
 router.get('/login', function(req, res, next) {
-  	res.render('login');
+    res.render('login');
 });
 
 router.get('/logout', function(req, res, next) {
-  	res.render('index');
+    res.render('index');
 });
 
 router.get('/signIn', function(req, res, next) {
- 	res.render('signIn');
+    res.render('signIn');
+
+});
+router.post('/signIn', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+
+    collection.insert({
+        "userId": req.body.userId,
+        "password": req.body.password
+    }, function(err, doc) {
+        if (err) {
+            res.send("user data is not inserted sorry");
+        } else {
+            res.redirect("/");
+        }
+    })
 });
 
 router.get('/edit', function(req, res, next) {
-  	res.render('edit');
+    res.render('edit');
 });
+router.get('/delete', function (req, res, next) {
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.remove({}, function (err) {
+		if (err) {res.send("user data is not inserted sorry");}
+		else{
+			res.redirect("/");
+		}
+	})
+})
 
 module.exports = router;
